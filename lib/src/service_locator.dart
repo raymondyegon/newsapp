a5729e0251c98c584da2678dd94374d11d08cd7b
raymondyegon <raymondyegon17@gmail.com>
2021-05-21 20:17:45 +0300

    Added function to save and remove from persistence storage

--- lib/src/service_locator.dart
+++ lib/src/service_locator.dart
@@ -1,5 +1,6 @@
 import 'package:get_it/get_it.dart';
 import 'package:logger/logger.dart';
+import 'package:newsapp/src/utils/sharedprefsutil.dart';
 
 GetIt sl = GetIt.instance;
 
@@ -7,4 +8,7 @@ GetIt sl = GetIt.instance;
 void setupServiceLocator() {
   // For pretty printing while debbuging
   sl.registerLazySingleton<Logger>(() => Logger(printer: PrettyPrinter()));
+
+  // For Saving and Getting items from DB
+  sl.registerLazySingleton<SharedPrefsUtil>(() => SharedPrefsUtil());
 }
---
+++ lib/src/utils/sharedprefsutil.dart
@@ -0,0 +1,45 @@
+import 'package:shared_preferences/shared_preferences.dart';
+
+class SharedPrefsUtil {
+  // Keys
+  static const String favorites = 'favorites';
+
+  // Function to save data
+  Future<void> set(String key, value) async {
+    SharedPreferences sharedPreferences = await SharedPreferences.getInstance();
+
+    if (value is bool) {
+      sharedPreferences.setBool(key, value);
+    } else if (value is String) {
+      sharedPreferences.setString(key, value);
+    } else if (value is double) {
+      sharedPreferences.setDouble(key, value);
+    } else if (value is int) {
+      sharedPreferences.setInt(key, value);
+    } else if (value is List<String>) {
+      sharedPreferences.setStringList(key, value);
+    }
+  }
+
+  // Function to load data
+  Future<dynamic> get(String key, {dynamic defaultValue}) async {
+    SharedPreferences sharedPreferences = await SharedPreferences.getInstance();
+    return await sharedPreferences.get(key) ?? defaultValue;
+  }
+
+  Future<void> setFavorite(String value) async {
+    List<String> userFav = await getFavorites() ?? <String>[];
+    userFav.add(value);
+    return await set(favorites, userFav);
+  }
+
+  Future<List<String>> getFavorites() async {
+    return await get(favorites, defaultValue: []);
+  }
+
+  Future<void> removeFavorite(String value) async {
+    List<String> userFav = await getFavorites() ?? <String>[];
+    userFav.remove(value);
+    return await set(favorites, userFav);
+  }
+}
