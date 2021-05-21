418e9389a3dbb59966ec0c4032822e2ac2bf0f2b
raymondyegon <raymondyegon17@gmail.com>
2021-05-21 10:33:45 +0300

    Added more colors

--- lib/src/common/theme.dart
+++ lib/src/common/theme.dart
@@ -4,10 +4,11 @@ class BaseTheme {
   Color primary = Color.fromRGBO(12, 57, 172, 1);
 
   Color background = Colors.white;
-  Color backgroundDark = Color.fromRGBO(17, 24, 39, 1);
+  Color gray900 = Color.fromRGBO(17, 24, 39, 1);
 
   Color gray50 = Color.fromRGBO(249, 250, 251, 1);
   Color gray100 = Color.fromRGBO(243, 244, 246, 1);
+  Color gray200 = Color.fromRGBO(229, 231, 235, 1);
   Color gray300 = Color.fromRGBO(209, 213, 219, 1);
   Color gray400 = Color.fromRGBO(156, 163, 175, 1);
   Color gray500 = Color.fromRGBO(107, 114, 128, 1);
