4369f033d569ec9d36c561d931a9c060498decc8
raymondyegon <raymondyegon17@gmail.com>
2021-05-21 10:06:48 +0300

    Added more text styles

--- lib/src/common/styles.dart
+++ lib/src/common/styles.dart
@@ -7,7 +7,7 @@ class AppStyles {
   // For selected bottom bar text
   static TextStyle textStyleSelected(BuildContext context) {
     return GoogleFonts.ibmPlexSans(
-      fontSize: 11.5.sp,
+      fontSize: 12.0.sp,
       fontWeight: FontWeight.w600,
       color: StateContainer.of(context).theme.gray800,
     );
@@ -16,9 +16,54 @@ class AppStyles {
   // For unselected bottom bar text
   static TextStyle textStyleUnselected(BuildContext context) {
     return GoogleFonts.ibmPlexSans(
-      fontSize: 11.5.sp,
+      fontSize: 12.0.sp,
       fontWeight: FontWeight.w400,
       color: StateContainer.of(context).theme.gray800,
+    );
+  }
+
+  // For title in selected page
+  static TextStyle textStyleTitleTop() {
+    return GoogleFonts.ibmPlexSans(
+      color: Colors.white,
+      fontSize: 16.0.sp,
+      fontWeight: FontWeight.w600,
+    );
+  }
+
+  // For not-found text while articles is empty
+  static TextStyle textStyleNoArticles(BuildContext context) {
+    return GoogleFonts.ibmPlexSans(
+      fontSize: 13.0.sp,
+      color: StateContainer.of(context).theme.gray800,
+      fontWeight: FontWeight.w600,
+    );
+  }
+
+  // For article feed title
+  static TextStyle textStyleFeedTitle(BuildContext context) {
+    return GoogleFonts.ibmPlexSans(
+      fontSize: 12.5.sp,
+      color: StateContainer.of(context).theme.gray800,
+      fontWeight: FontWeight.w600,
+    );
+  }
+
+  // For article feed date
+  static TextStyle textStyleFeedDate(BuildContext context) {
+    return GoogleFonts.ibmPlexSans(
+      color: StateContainer.of(context).theme.gray500,
+      fontSize: 10.5.sp,
+      fontWeight: FontWeight.w400,
+    );
+  }
+
+  // For page description
+  static TextStyle textStylePageDescription(BuildContext context) {
+    return GoogleFonts.ibmPlexSans(
+      color: StateContainer.of(context).theme.gray800,
+      fontSize: 15.0.sp,
+      fontWeight: FontWeight.w700,
     );
   }
 }
